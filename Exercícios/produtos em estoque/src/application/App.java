a19a169d77244812242b53c6f68d8167ccbcee77
thiagoDRangel <info.tec.campos@gmail.com>
2022-12-23 12:08:54 -0300

    Adicionando funcionalidades add/remove in stock

--- Exercícios/produtos em estoque/src/application/App.java
+++ Exercícios/produtos em estoque/src/application/App.java
@@ -18,12 +18,25 @@ public class App {
     System.out.print("Quantity in stock: ");
     product.quantity = sc.nextInt();
 
-    System.out.println(product.name
-      + ", "
-      + product.price
-      + ", "
-      + product.quantity
-    );
+    System.out.println();
+    System.out.println("Product data: " + product);
+
+    System.out.println();
+    System.out.print("Enter the number of products to be added in stock: ");
+    int quantity = sc.nextInt();
+    product.addProducts(quantity);
+
+    System.out.println();
+    System.out.println("Update data: " + product);
+
+    System.out.println();
+    System.out.print("Enter the number of products to be remove from stock: ");
+    quantity = sc.nextInt();
+    product.removeProducts(quantity);
+
+    System.out.println();
+    System.out.println("Update data: " + product);
+    
     sc.close();
   }
 }
