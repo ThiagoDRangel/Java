8de53c88b4e0b031d42dcdb354a80eed91896077
ThiagoDRangel <info.tec.campos@gmail.com>
2023-01-04 16:48:45 -0300

    Adicionando as classes Board e Piece

--- Games/jogo de xadrez/src/app/Program.java
+++ Games/jogo de xadrez/src/app/Program.java
@@ -1,13 +1,11 @@
 package app;
 
-import boardgame.Position;
+import boardgame.Board;
 
 public class Program {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
-        Position pos = new Position(3, 5);
-        System.out.println(pos);
+        Board board = new Board(8, 8);
     }
 }
