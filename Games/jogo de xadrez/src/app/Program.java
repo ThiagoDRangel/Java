11430139ce02e230a966c7eda0c2dea9b5e90067
ThiagoDRangel <info.tec.campos@gmail.com>
2023-01-04 16:19:49 -0300

    Adicionando a classe Position

--- Games/jogo de xadrez/src/app/Program.java
+++ Games/jogo de xadrez/src/app/Program.java
@@ -1,9 +1,13 @@
 package app;
 
+import boardgame.Position;
+
 public class Program {
 
-public static void main(String[] args) {
+    public static void main(String[] args) {
 
-    System.out.println("Hello world!");
-}
+        System.out.println("Hello world!");
+        Position pos = new Position(3, 5);
+        System.out.println(pos);
+    }
 }
