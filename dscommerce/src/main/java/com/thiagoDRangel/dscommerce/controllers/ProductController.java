7b08a2791f5300e2c3a782fd7231405e3a322f14
ThiagoDRangel <info.tec.campos@gmail.com>
2023-06-15 10:46:21 -0300

    Configurando o método POST - adicionando um novo produto

--- dscommerce/src/main/java/com/thiagoDRangel/dscommerce/controllers/ProductController.java
+++ dscommerce/src/main/java/com/thiagoDRangel/dscommerce/controllers/ProductController.java
@@ -6,10 +6,8 @@ import com.thiagoDRangel.dscommerce.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -28,5 +26,10 @@ public class ProductController {
     @GetMapping
     public Page<ProductDTO> findAll(Pageable pageable) {
         return service.findAll(pageable);
+    }
+
+    @PostMapping
+    public ProductDTO insert(@RequestBody ProductDTO dto) {
+        return service.insert(dto);
     }
 }
