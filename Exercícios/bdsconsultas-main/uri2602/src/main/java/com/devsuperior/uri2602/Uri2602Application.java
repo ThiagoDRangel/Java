8a5f4ece594f5b55a0b216ba8d26889d9ee70907
ThiagoDRangel <info.tec.campos@gmail.com>
2023-06-22 11:08:03 -0300

    Trabalhando SQL raiz e JPA

---
+++ Exercícios/bdsconsultas-main/uri2602/src/main/java/com/devsuperior/uri2602/Uri2602Application.java
@@ -0,0 +1,34 @@
+package com.devsuperior.uri2602;
+
+import com.devsuperior.uri2602.dto.CustomerMinDTO;
+import com.devsuperior.uri2602.projections.CustomerMinProjection;
+import com.devsuperior.uri2602.repositories.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@SpringBootApplication
+public class Uri2602Application implements CommandLineRunner {
+
+	@Autowired
+	private CustomerRepository repository;
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(Uri2602Application.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws  Exception {
+		List<CustomerMinProjection> list = repository.search1("RS");
+		List<CustomerMinDTO> result1 = list.stream().map(x -> new CustomerMinDTO(x)).collect(Collectors.toList());
+
+		for (CustomerMinDTO obj : result1) {
+			System.out.println(obj);
+		}
+	}
+}
