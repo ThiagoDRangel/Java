ab66197478c78d18f0a075ae7413a944eef77c07
ThiagoDRangel <info.tec.campos@gmail.com>
2023-07-18 11:19:24 -0300

    Adicionando roles e controle de acesso aos usuários

--- dscommerce/src/main/java/com/thiagoDRangel/dscommerce/entities/User.java
+++ dscommerce/src/main/java/com/thiagoDRangel/dscommerce/entities/User.java
@@ -3,9 +3,7 @@ package com.thiagoDRangel.dscommerce.entities;
 import jakarta.persistence.*;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 
 
 @Entity
@@ -24,8 +22,15 @@ public class User {
 
     @OneToMany(mappedBy = "client")
     private List<Order> orders = new ArrayList<>();
+    @ManyToMany
+    @JoinTable(name = "tb_user_role",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles = new HashSet<>();
+
     public User() {
     }
+
 
     public User(
             Long id,
@@ -93,6 +98,23 @@ public class User {
 
     public List<Order> getOrders() {
         return orders;
+    }
+
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    public void addRole(Role role) {
+        roles.add(role);
+    }
+
+    public boolean hasRole (String roleName) {
+        for (Role role: roles) {
+            if (role.getAuthority().equals(roleName)) {
+                return true;
+            }
+        }
+        return false;
     }
 
     @Override
